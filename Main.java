f7cea5e7f6aa632bed54a1975e54aaa5234b2514
Vedansh Shriva <vedanshshrivas66@gmail.com>
2026-06-09 23:37:38 +0530

    test

---
+++ Main.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter your roll Number: ");
+        int rollNumber = input.nextInt();
+        System.out.println("Your roll number is: " + rollNumber);
+    }
+}
